b123787c0639d888dfbb12ded33a568533310163
BnnQ <1v25fa94@gmail.com>
2023-11-28 01:05:36 +0200

    Added user manager service

--- src/services/api-user-manager.service.ts
+++ src/services/api-user-manager.service.ts
@@ -5,105 +5,105 @@ import IHttpService from "./abstractions/i-http-service";
 import {User} from "../models/user";
 import {UserLoginDto} from "../models/dto/user-login-dto";
 import {UserRegisterDto} from "../models/dto/user-register-dto";
-import {UserEditDto} from "../models/dto/user-edit-dto";
 import {SERVICE_IDENTIFIERS} from "../app/app.config";
 import {environment} from "../environments/environment";
+import {AxiosResponse} from "axios";
 
-@Injectable({ providedIn: 'root' })
-export class ApiUserManagerService implements IUserManager {
-  private readonly serverApiUrl: string = environment.serverApiUrl;
-  public isCurrentUserAuthenticated: BehaviorSubject<boolean> =
-    new BehaviorSubject<boolean>(false);
+@Injectable({providedIn: 'root'})
+export class ApiUserManagerService implements IUserManager
+{
+    private readonly serverApiUrl: string = environment.serverApiUrl;
+    public isCurrentUserAuthenticated: BehaviorSubject<boolean> =
+        new BehaviorSubject<boolean>(false);
 
-  constructor(
-    @Inject(SERVICE_IDENTIFIERS.IHttpService)
-    private readonly httpService: IHttpService
-  ) {
-    this.updateAuthenticationStatus();
-  }
+    constructor(
+        @Inject(SERVICE_IDENTIFIERS.IHttpService)
+        private readonly httpService: IHttpService
+    ) { }
 
-  private updateAuthenticationStatus(): void {
-    this.getIsCurrentUserAuthenticated().then(isCurrentUserAuthenticated =>
-      this.setIsAuthenticated(isCurrentUserAuthenticated));
-  }
+    private setIsAuthenticated(isAuthenticated: boolean): void
+    {
+        this.isCurrentUserAuthenticated!.next(isAuthenticated);
+    }
 
-  private setIsAuthenticated(isAuthenticated: boolean): void {
-    this.isCurrentUserAuthenticated!.next(isAuthenticated);
-  }
+    getUser(username: string): Promise<User>
+    {
+        return this.httpService.get(
+            new URL(`${this.serverApiUrl}user/get/byUsername`),
+            {username: username}
+        );
+    }
 
-  getUser(username: string): Promise<User> {
-    return this.httpService.get(
-      new URL(`${this.serverApiUrl}Account/GetUser`),
-      { username: username }
-    );
-  }
+    async getCurrentUser(): Promise<User>
+    {
+        const currentUserId = await this.getCurrentUserId();
+        return await this.getUserById(currentUserId);
+    }
 
-  async getCurrentUser(): Promise<User> {
-    const currentUserId = await this.getCurrentUserId();
-    return await this.getUserById(currentUserId);
-  }
+    async getCurrentUserId(): Promise<number>
+    {
+      return await this.httpService.get(new URL(`${this.serverApiUrl}auth/get/currentUserId`));
+    }
 
-  async getCurrentUserId(): Promise<string> {
-    return (
-      (await this.httpService.get(
-        new URL(`${this.serverApiUrl}Account/GetCurrentUserId`)
-      )) as {
-        currentUserId: string;
-      }
-    ).currentUserId;
-  }
+    getUserById(id: number): Promise<User>
+    {
+        return this.httpService.get(
+            new URL(`${this.serverApiUrl}user/get/byId`),
+            {id: id}
+        );
+    }
 
-  private async getIsCurrentUserAuthenticated(): Promise<boolean> {
-    const response = await this.httpService.get(
-      new URL(`${this.serverApiUrl}Account/IsAuthenticated`)
-    );
-    return (response as { isAuthenticated: boolean }).isAuthenticated;
-  }
+    async login(loginDto: UserLoginDto): Promise<string | void>
+    {
+      console.log('loginDto:', loginDto);
+      const encodedCredentials = btoa(`${loginDto.email}:${loginDto.password}`);
+        await this.httpService.post(
+            new URL(`${this.serverApiUrl}auth/login`),
+            undefined,
+            undefined,
+            undefined,
+          {
+            Authorization: `Basic ${encodedCredentials}`
+            },
+          undefined,
+            (response: AxiosResponse) =>
+            {
+                if (response.status === 200)
+                {
+                    console.log('success');
+                    console.log('token', response.data);
+                    localStorage.setItem('token', response.data);
+                    this.setIsAuthenticated(true);
+                }
+            }
+        );
+        return Promise.resolve();
+    }
 
-  getUserById(id: string): Promise<User> {
-    return this.httpService.get(
-      new URL(`${this.serverApiUrl}Account/GetUserById`),
-      { id: id }
-    );
-  }
-
-  async login(loginDto: UserLoginDto): Promise<string | void> {
+    async register(registerDto: UserRegisterDto): Promise<string | void>
+    {
       await this.httpService.post(
-        new URL(`${this.serverApiUrl}Account/Login`),
-        loginDto
-      );
+            new URL(`${this.serverApiUrl}auth/register`),
+            registerDto,
+          undefined,
+          undefined,
+          undefined,
+          undefined,
+          (response: AxiosResponse) =>
+          {
+              if (response.status === 200)
+              {
+                  this.login(new UserLoginDto(registerDto.email, registerDto.password));
+              }
+          }
+        );
+    }
 
-      this.updateAuthenticationStatus();
-  }
-
-  async register(registerDto: UserRegisterDto): Promise<string | void> {
-      await this.httpService.post(
-        new URL(`${this.serverApiUrl}Account/Register`),
-        registerDto
-      );
-
-      this.updateAuthenticationStatus();
-  }
-
-  async logout(): Promise<void> {
-    await this.httpService.get(new URL(`${this.serverApiUrl}Account/Logout`));
-    this.updateAuthenticationStatus();
-  }
-
-  async editUserProfile(userEditDto: UserEditDto): Promise<void> {
-    const body: any = {
-      fullName: userEditDto.fullName,
-      avatar: userEditDto.avatar
-    };
-
-    await this.httpService
-      .post(
-        new URL(`${this.serverApiUrl}Account/EditUserProfile`),
-        body,
-        { id: await this.getCurrentUserId() },
-        undefined,
-        { 'Content-Type': 'multipart/form-data' }
-      );
-  }
+    logout(): Promise<void>
+    {
+        localStorage.removeItem('token');
+        this.setIsAuthenticated(false);
+        return Promise.resolve();
+    }
 
 }
