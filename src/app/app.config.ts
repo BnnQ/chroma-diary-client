d8c26a954509d4e843d68bd67d0e4141b2f57a12
BnnQ <1v25fa94@gmail.com>
2023-11-28 01:06:46 +0200

    Registered services

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,19 +1,23 @@
 import {ApplicationConfig, InjectionToken} from '@angular/core';
-import { provideRouter } from '@angular/router';
-import { Dictionary } from '../models/abstractions/dictionary';
+import {provideRouter} from '@angular/router';
+import {Dictionary} from '../models/abstractions/dictionary';
 
-import { routes } from './app.routes';
+import {routes} from './app.routes';
 import IHttpService from "../services/abstractions/i-http-service";
 import {IUserManager} from "../services/abstractions/i-user-manager";
 import {AxiosHttpService} from "../services/axios-http.service";
-import {StubUserManagerService} from "../services/stub-user-manager.service";
 import {ScreenService} from "../services/screen.service";
 import {GlobalAttributeService} from "../services/global-attribute.service";
 import {RouteTrackerService} from "../services/route-tracker.service";
 import {IDiaryRepository} from "../services/abstractions/i-diary-repository";
-import {StubDiaryRepository} from "../services/stub-diary-repository.service";
 import {ITagRepository} from "../services/abstractions/i-tag-repository";
-import {StubTagRepositoryService} from "../services/stub-tag-repository.service";
+import {IPermissionManager} from "../services/abstractions/i-permission-manager";
+import {IPostRepository} from "../services/abstractions/i-post-repository";
+import {ApiDiaryRepository} from "../services/api-diary-repository.service";
+import {ApiPostRepository} from "../services/api-post-repository.service";
+import {ApiTagRepository} from "../services/api-tag-repository";
+import {ApiUserManagerService} from "../services/api-user-manager.service";
+import {ApiPermissionManager} from "../services/api-permission-manager";
 
 export const SERVICE_IDENTIFIERS = {
   IHttpService: new InjectionToken<IHttpService>('IHttpService'),
@@ -23,10 +27,12 @@ export const SERVICE_IDENTIFIERS = {
   GlobalAttributeService: new InjectionToken<GlobalAttributeService>('GlobalAttributeService'),
   RouteTrackerService: new InjectionToken<RouteTrackerService>('RouteTrackerService'),
   IDiaryRepository: new InjectionToken<IDiaryRepository>('IDiaryRepository'),
-  ITagRepository: new InjectionToken<ITagRepository>('ITagRepository')
+  ITagRepository: new InjectionToken<ITagRepository>('ITagRepository'),
+  IPermissionManager: new InjectionToken<IPermissionManager>('IPermissionsManager'),
+  IPostRepository: new InjectionToken<IPostRepository>('IPostRepository'),
 };
 
-export const ATTRIBUTES : Dictionary<string, boolean> = {
+export const ATTRIBUTES: Dictionary<string, boolean> = {
   'sidebar-locked': true,
   'sidebar-collapsed': false,
   'sidebar-disabled': false
@@ -36,20 +42,23 @@ export const appConfig: ApplicationConfig = {
   providers:
     [
       provideRouter(routes),
-      { provide: SERVICE_IDENTIFIERS.IHttpService, useClass: AxiosHttpService },
-      { provide: SERVICE_IDENTIFIERS.IUserManager, useClass: StubUserManagerService },
-      { provide: SERVICE_IDENTIFIERS.ScreenService, useClass: ScreenService },
-      { provide: SERVICE_IDENTIFIERS.AttributeDefinition, useValue: ATTRIBUTES },
+      {provide: SERVICE_IDENTIFIERS.IHttpService, useClass: AxiosHttpService},
+      {provide: SERVICE_IDENTIFIERS.ScreenService, useClass: ScreenService},
+      {provide: SERVICE_IDENTIFIERS.AttributeDefinition, useValue: ATTRIBUTES},
       {
         provide: SERVICE_IDENTIFIERS.GlobalAttributeService,
-        useFactory: (attributeDefinition : Dictionary<string, boolean>) => {
+        useFactory: (attributeDefinition: Dictionary<string, boolean>) =>
+        {
           return new GlobalAttributeService(attributeDefinition);
         },
         deps: [SERVICE_IDENTIFIERS.AttributeDefinition]
       },
-      { provide: SERVICE_IDENTIFIERS.RouteTrackerService, useClass: RouteTrackerService },
-      { provide: SERVICE_IDENTIFIERS.IDiaryRepository, useClass: StubDiaryRepository },
-      { provide: SERVICE_IDENTIFIERS.ITagRepository, useClass: StubTagRepositoryService }
-  ],
+      {provide: SERVICE_IDENTIFIERS.RouteTrackerService, useClass: RouteTrackerService},
+      {provide: SERVICE_IDENTIFIERS.IDiaryRepository, useClass: ApiDiaryRepository},
+      {provide: SERVICE_IDENTIFIERS.ITagRepository, useClass: ApiTagRepository},
+      {provide: SERVICE_IDENTIFIERS.IUserManager, useClass: ApiUserManagerService},
+      {provide: SERVICE_IDENTIFIERS.IPermissionManager, useClass: ApiPermissionManager},
+      {provide: SERVICE_IDENTIFIERS.IPostRepository, useClass: ApiPostRepository}
+    ],
 };
 
