8ec24a089e8e03c92b708251ba6977ba6a6d5c42
BnnQ <1v25fa94@gmail.com>
2023-11-28 01:09:13 +0200

    Registered custom pipes

--- src/modules/custom-pipes.module.ts
+++ src/modules/custom-pipes.module.ts
@@ -2,10 +2,14 @@ import {NgModule} from "@angular/core";
 import {ContrastColorPipe} from "../pipes/contrast-color.pipe";
 import {CommonModule} from "@angular/common";
 import {CountdownDateDaysPipe} from "../pipes/countdown-date-days.pipe";
+import {ContainsMediaAttachmentPipe} from "../pipes/contains-media-attachment.pipe";
+import {ContainsFileAttachmentPipe} from "../pipes/contains-file-attachment.pipe";
+import {GetAttachmentClassesPipe} from "../pipes/get-attachment-classes.pipe";
+import {FileSizePipe} from "../pipes/file-size.pipe";
 
 @NgModule({
-  declarations: [ContrastColorPipe, CountdownDateDaysPipe],
+  declarations: [ContrastColorPipe, CountdownDateDaysPipe, ContainsMediaAttachmentPipe, ContainsFileAttachmentPipe, GetAttachmentClassesPipe, FileSizePipe],
   imports: [CommonModule],
-  exports: [ContrastColorPipe, CountdownDateDaysPipe]
+  exports: [ContrastColorPipe, CountdownDateDaysPipe, ContainsMediaAttachmentPipe, ContainsFileAttachmentPipe, GetAttachmentClassesPipe, FileSizePipe]
 })
 export class CustomPipesModule { }
