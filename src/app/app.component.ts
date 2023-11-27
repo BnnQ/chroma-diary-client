bb96e53f7ea5d9c9fb9e266d56bd3b1d14b68de5
BnnQ <1v25fa94@gmail.com>
2023-11-28 01:04:20 +0200

    Fixed app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,11 @@
 import {Component, Inject, OnInit} from '@angular/core';
 import {CommonModule} from '@angular/common';
-import {ActivatedRoute, NavigationEnd, Router, RouterModule, RouterOutlet} from '@angular/router';
+import {Router, RouterModule, RouterOutlet} from '@angular/router';
 import {SERVICE_IDENTIFIERS} from "./app.config";
 import {User} from "../models/user";
 import {IUserManager} from "../services/abstractions/i-user-manager";
 import {ScreenModule} from "../modules/screen.module";
-import {ObservableItem} from "../models/observable-item";
-import {ObservableArray} from "../models/observable-array";
-import {HtmlAttribute} from "../models/html-attribute";
 import {GlobalAttributeService} from "../services/global-attribute.service";
-import {DelayHelper} from "../utils/delay-helper";
-import {filter} from "rxjs";
 import {RouteTrackerService} from "../services/route-tracker.service";
 import {routes} from "./app.routes";
 import {CustomPipesModule} from "../modules/custom-pipes.module";
@@ -22,7 +17,7 @@ import {CustomDirectivesModule} from "../modules/custom-directives.module";
     imports: [CommonModule, RouterOutlet, RouterModule, ScreenModule, CustomPipesModule, CustomDirectivesModule],
     templateUrl: './app.component.html'
 })
-export class AppComponent
+export class AppComponent implements OnInit
 {
     title = 'ChromaDiary';
     user?: User;
@@ -33,10 +28,16 @@ export class AppComponent
         @Inject(SERVICE_IDENTIFIERS.GlobalAttributeService)
         private readonly globalAttributeService: GlobalAttributeService,
         @Inject(SERVICE_IDENTIFIERS.RouteTrackerService)
-        public readonly routeTrackerService: RouteTrackerService
+        public readonly routeTrackerService: RouteTrackerService,
+        private readonly router : Router
     )
     {
+    }
 
+    async ngOnInit()
+    {
+        //await this.logout();
+        this.user = await this.userManager.getCurrentUser();
     }
 
     toggleSidebar()
@@ -62,6 +63,14 @@ export class AppComponent
     enableSidebar()
     {
       this.globalAttributeService.setAttribute('sidebar-disabled', false);
+    }
+
+    async logout()
+    {
+        await this.userManager.logout();
+        this.user = undefined;
+        await this.router.navigate(['login']);
+
     }
 
     protected readonly routes = routes;
